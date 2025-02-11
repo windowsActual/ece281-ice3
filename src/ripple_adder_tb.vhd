7c5abf5dbbfefaaac3c450275857e317836fcdb4
windowsActual <177792113+windowsActual@users.noreply.github.com>
2025-02-10 22:30:34 -0700

    pushing png files

--- src/ripple_adder_tb.vhd
+++ src/ripple_adder_tb.vhd
@@ -43,16 +43,19 @@ begin
 	begin
 	
 	   -- Test all zeros input
+	   -- not my stuff here, Gavin Smith helped me out here
 	   w_addends <= x"00"; w_Cin <= '0'; wait for 10 ns;
 	       assert (w_sum = x"0" and w_Cout = '0') report "bad with zeros" severity failure;
        -- Test all ones input
        w_addends <= x"FF"; w_Cin <= '1'; wait for 10 ns;
 	       assert (w_sum = x"E" and w_Cout = '1') report "bad with ones" severity failure;
        -- TODO, a few other test cases
-       w_addends <=x"1"; w_Cin <= '1'; wait for 10 ns;
-           assert (w_sum = x"1" and w_Cout = '0') report "bad with 0001" severity failure;
-       w_addends <= x"3"; w_Cin <= '0'; wait for 10 ns;
-           assert (w_sum = x"2" and w_Cout = '1') report "bad with 0011" severity failure;
+       w_addends <= x"00"; w_Cin <= '1'; wait for 10 ns;
+           assert (w_sum = x"1" and w_Cout = '0') report "bad with x00 1" severity failure;
+       w_addends <= x"DE"; w_Cin <= '1'; wait for 10 ns;
+           assert (w_sum = x"C" and w_Cout = '1') report "bad with xDE 1" severity failure;
+       w_addends <= x"BC"; w_Cin <= '0'; wait for 10 ns;
+           assert (w_sum = x"7" and w_Cout = '1') report "bad with xBC 0" severity failure;
 		wait; -- wait forever
 	end process;	
 	-----------------------------------------------------	
