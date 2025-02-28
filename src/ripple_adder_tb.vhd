fdfce23da78bfceddc97466eea7f06ca934dad0c
windowsActual <177792113+windowsActual@users.noreply.github.com>
2025-02-28 13:43:05 -0700

    update

--- src/ripple_adder_tb.vhd
+++ src/ripple_adder_tb.vhd
@@ -48,7 +48,7 @@ begin
 	       assert (w_sum = x"0" and w_Cout = '0') report "bad with zeros" severity failure;
        -- Test all ones input
        w_addends <= x"FF"; w_Cin <= '1'; wait for 10 ns;
-	       assert (w_sum = x"E" and w_Cout = '1') report "bad with ones" severity failure;
+	       assert (w_sum = x"F" and w_Cout = '1') report "bad with ones" severity failure;
        -- TODO, a few other test cases
        w_addends <= x"00"; w_Cin <= '1'; wait for 10 ns;
            assert (w_sum = x"1" and w_Cout = '0') report "bad with x00 1" severity failure;
